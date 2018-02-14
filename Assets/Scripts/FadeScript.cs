d7290e5b48617b3b2725cc20b0b805afdd4c731a
eslowikowski <erik.slowikowski@channelpilot.com>
2018-02-14 10:06:04 +0100

    added FadeIn

--- Assets/Scripts/FadeScript.cs
+++ Assets/Scripts/FadeScript.cs
@@ -4,21 +4,64 @@ using UnityEngine;
 
 public class FadeScript : MonoBehaviour {
 
-	public void FadeMe()
+    private bool _haltIn = false;
+    private bool _haltOut = false;
+
+	public void FadeOut()
     {
-        StartCoroutine(DoFade());
+        _haltIn = true;
+        _haltOut = false;
+        StartCoroutine(DoFadeOut());
     }
 
-    IEnumerator DoFade()
+    IEnumerator DoFadeOut()
     {
         CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        if (_haltOut)
+        {
+            yield break;
+        }
+
         while (canvasGroup.alpha > 0)
         {
+            if (_haltOut)
+            {
+                yield break;
+            }
             canvasGroup.alpha -= Time.deltaTime / 2;
             yield return null;
         }
 
         canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    public void FadeIn()
+    {
+        _haltIn = false;
+        _haltOut = true;
+        StartCoroutine(DoFadeIn());
+    }
+
+    IEnumerator DoFadeIn()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        if (_haltIn)
+        {
+            yield break;
+        }
+
+        while (canvasGroup.alpha < 1)
+        {
+            if (_haltIn)
+            {
+                yield break;
+            }
+            canvasGroup.alpha += Time.deltaTime / 2;
+            yield return null;
+        }
+
+        canvasGroup.interactable = true;
         yield return null;
     }
 }
