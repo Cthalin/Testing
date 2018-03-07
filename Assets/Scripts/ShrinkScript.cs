3f5551cac7162c451bf08e16f9c6e2b13a93c580
Erik <elidorian@gmail.com>
2018-03-07 20:05:15 +0100

    3d scene and shrink added

---
+++ Assets/Scripts/ShrinkScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _shrinkObject;
+
+    [SerializeField]
+    private float _howLongToShrink = 0.2f;
+
+    public void Shrink()
+    {
+        StartCoroutine(DoShrink());
+    }
+
+    IEnumerator DoShrink()
+    {
+        float timer = Time.time + _howLongToShrink;
+
+        while (Time.time < timer)
+        {
+            _shrinkObject.transform.localScale = new Vector3(0.99f * _shrinkObject.transform.localScale.x, 0.99f * _shrinkObject.transform.localScale.y, 0.99f * _shrinkObject.transform.localScale.z) ;
+
+            yield return null;
+        }
+
+        //canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    public void Enlarge()
+    {
+        StartCoroutine(DoEnlarge());
+    }
+
+    IEnumerator DoEnlarge()
+    {
+        float timer = Time.time + _howLongToShrink;
+
+        while (Time.time < timer)
+        {
+            _shrinkObject.transform.localScale = new Vector3(1.01f * _shrinkObject.transform.localScale.x, 1.01f * _shrinkObject.transform.localScale.y, 1.01f * _shrinkObject.transform.localScale.z);
+
+            yield return null;
+        }
+
+        //canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown("down"))
+        {
+            Shrink();
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            Enlarge();
+        }
+	}
+}
