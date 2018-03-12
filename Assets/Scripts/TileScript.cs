596cb8675ae2c4ca937bbfcc67bb0fe8b5c9f10a
Cthalin <elidorian@gmail.com>
2018-03-12 15:31:54 +0100

    VanishScript for Tile Sequence

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    [SerializeField] GameObject[] Tiles = new GameObject[3];
+    private int _tileNo;
+    private string _tileName;
+
+    private void OnEnable()
+    {
+        _tileNo = Tiles.Length -1;
+        _tileName = Tiles[_tileNo].name;
+    }
+
+    public void SetTileAsTarget(int tileNo)
+    {
+        Tiles[tileNo].tag = "target";
+    }
+
+    private void IterateOnTiles()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit _hit;
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit, Mathf.Infinity))
+            {
+                if (_hit.collider.tag == "target" && _hit.collider.name == _tileName)
+                {
+                    Vanish(_hit.collider.gameObject);
+                    if(_tileNo > 0)
+                    {
+                        _tileNo -= 1;
+                        _tileName = Tiles[_tileNo].name;
+                        SetTileAsTarget(_tileNo);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Vanish(GameObject gameObject)
+    {
+        StartCoroutine(DoVanish(gameObject));
+    }
+
+    IEnumerator DoVanish(GameObject gameObject)
+    {
+        while (gameObject.activeSelf)
+        {
+            gameObject.transform.localScale = new Vector3(0.9f * gameObject.transform.localScale.x, 0.9f * gameObject.transform.localScale.y, 0.9f * gameObject.transform.localScale.z);
+
+            if (gameObject.transform.localScale.x <= 0.1f)
+            {
+                gameObject.SetActive(false);
+            }
+
+            yield return null;
+        }
+
+        //canvasGroup.interactable = false;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/VanishScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishScript : MonoBehaviour {
+
+    public void Vanish()
+    {
+        StartCoroutine(DoVanish());
+    }
+
+    IEnumerator DoVanish()
+    {
+        while (gameObject.activeSelf)
+        {
+            gameObject.transform.localScale = new Vector3(0.9f * gameObject.transform.localScale.x, 0.9f * gameObject.transform.localScale.y, 0.9f * gameObject.transform.localScale.z) ;
+
+            if(gameObject.transform.localScale.x <= 0.1f)
+            {
+                gameObject.SetActive(false);
+            }
+
+            yield return null;
+        }
+
+        //canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        /*
+         * For testing purposes using Down key and Mouse 0 Button
+         * 
+         */
+         
+        if (Input.GetKeyDown("down"))
+        {
+            Vanish();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit _hit;
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray,out _hit, Mathf.Infinity))
+            {
+                if(_hit.collider.tag == "target")
+                {
+                    _hit.collider.GetComponent<VanishScript>().Vanish();
+                }
+            }
+        }
+    }
+}
