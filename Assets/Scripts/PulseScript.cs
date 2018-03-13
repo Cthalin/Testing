6f94bca698b92256522440b2af637aa3603445f9
Cthalin <elidorian@gmail.com>
2018-03-13 11:23:46 +0100

    Pulse script + object

---
+++ Assets/Scripts/PulseScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PulseScript : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.tag == "testTag")
+                {
+                    hit.collider.GetComponent<Animator>().enabled = true;
+                }
+            }
+        }
+    }
+}
